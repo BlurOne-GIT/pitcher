5eb4566dcde0d4768a70c9404e4911d608891eba
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2026-01-31 21:06:50 -0300

    feat: device enumeration

--- main.c
+++ main.c
@@ -113,7 +113,7 @@ void data_callback(ma_device *pDevice, void *pOutput, const void *pInput, ma_uin
 }
 
 int main(int argc, char *argv[]) {
-  // TODO: char **device_name = flag_str("-device", NULL, "Use a specific device for the detection.");
+  uint64_t *device_id = flag_uint64("-device", 0, "Use a specific device for the detection.");
   uint64_t *sample_rate = flag_uint64("-sampleRate", 0, "The sample rate to use for the device. It uses the device's prefered sample rate by default.");
   uint64_t *fft_rate_pow = flag_uint64("-fftRate", 13, "2^value of samples to use for the fft.");
   float *fmin = flag_float("-highpass", 80, "Lowest frequency (in Hz) for the high-pass filter.");
@@ -162,12 +162,42 @@ int main(int argc, char *argv[]) {
   device_config.sampleRate = *sample_rate;
   device_config.pUserData = &userdata;
   
-  // TODO: device enumeration
+  ma_context context;
+  result = ma_context_init(NULL, 0, NULL, &context);
+  if (result != MA_SUCCESS) {
+    fprintf(stderr, "Error %d creating context\n", result);
+    return 2;
+  }
+  
+  if (*device_id != 0) {
+    ma_device_info* pCaptureInfos;
+    ma_uint32 captureCount;
+    result = ma_context_get_devices(&context, NULL, NULL, &pCaptureInfos, &captureCount);
+    if (result != MA_SUCCESS) {
+      fprintf(stderr, "Error %d getting devices\n", result);
+      return 3;
+    }
+
+    if (*device_id >= captureCount) {
+      fprintf(stderr, "Device #%lu not found.\n", *device_id);
+      printf("Avaiable devices:\n");
+      for (ma_uint32 iDevice = 0; iDevice < captureCount; iDevice++) {
+        if (pCaptureInfos[iDevice].isDefault)
+          printf("(Default)");
+        else
+          printf("         ");
+        printf(" %d. %s\n", iDevice+1, pCaptureInfos[iDevice].name);
+      }
+      return 4;
+    }
+
+    device_config.capture.pDeviceID = &pCaptureInfos[*device_id-1].id;
+  }
   
   ma_device device;
-  result = ma_device_init(NULL, &device_config, &device);
+  result = ma_device_init(&context, &device_config, &device);
   if (result != MA_SUCCESS) {
-    printf("Error %d opening device", result);
+    fprintf(stderr, "Error %d opening device\n", result);
     return 1;
   }
 
